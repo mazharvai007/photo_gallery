8f41e39bbb24f198f8c1be8596edc2bc82a0adfb
swopnil <swopnil420@gmail.com>
2019-11-09 19:26:06 +0600

    Create this query method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -21,7 +21,7 @@
                     echo $row['username'] . "<br>";
                 }
 
-                // Found User
+                // Found User by id
                 $found_user = User::find_user_by_id(2);
                 echo $found_user['username'];
             ?>
--- admin/includes/user.php
+++ admin/includes/user.php
@@ -4,18 +4,20 @@ class User
 {
     // Find all users
     public static function find_all_users() {
-        global $database;
-
-        $result_set = $database->query("SELECT * FROM users");
-        return $result_set;
+        return self::find_this_query("SELECT * FROM users");
     }
 
+    // Find user by ID
     public static function find_user_by_id($user_id) {
-        global $database;
-
-        $result = $database->query("SELECT * FROM users WHERE user_id = $user_id LIMIT 1");
+        $result = self::find_this_query("SELECT * FROM users WHERE user_id = $user_id LIMIT 1");
         $found_user = mysqli_fetch_array($result);
         return $found_user;
+    }
+
+    public static function find_this_query($sql) {
+        global $database;
+        $result = $database->query($sql);
+        return $result;
     }
 }
 
