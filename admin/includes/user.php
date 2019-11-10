d68f2fbfce35c84d9d0a71ccf5869fd120216808
swopnil <swopnil420@gmail.com>
2019-11-11 00:23:19 +0600

    Short way auto instantiation

--- admin/includes/user.php
+++ admin/includes/user.php
@@ -28,14 +28,24 @@ class User
     }
 
     // Instantiate method
-    public static function instantiation($found_user) {
+    public static function instantiation($the_record) {
         $the_object = new self();
 
-        $the_object->user_id = $found_user['user_id'];
-        $the_object->username = $found_user['username'];
-        $the_object->password = $found_user['password'];
-        $the_object->first_name = $found_user['first_name'];
-        $the_object->last_name = $found_user['last_name'];
+//        $the_object->user_id = $found_user['user_id'];
+//        $the_object->username = $found_user['username'];
+//        $the_object->password = $found_user['password'];
+//        $the_object->first_name = $found_user['first_name'];
+//        $the_object->last_name = $found_user['last_name'];
+
+        /*
+         * Using foreach loop to get users data to short way
+         */
+
+        foreach ($the_record as $the_attribute => $value) {
+            if ($the_object->has_the_attribute($the_attribute)) {
+                $the_object->the_attribute = $value;
+            }
+        }
 
         return $the_object;
     }
