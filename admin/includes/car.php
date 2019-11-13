b4df8b1434bb43f3690694b23565b3aed39df529
swopnil <swopnil420@gmail.com>
2019-11-13 23:06:39 +0600

    Updating autoload function and test with Car class

---
+++ admin/includes/car.php
@@ -0,0 +1,8 @@
+<?php
+
+class Car
+{
+    public function run() {
+        echo "<h1>Hello, I'm running</h1>";
+    }
+}
--- admin/includes/functions.php
+++ admin/includes/functions.php
@@ -1,9 +1,13 @@
 <?php
 
 // Make autoload function to check class is exist?
-function __autoload($class) {
+function classAutoLoader($class) {
     $the_class = strtolower($class);
     $the_path = "includes/{$the_class}.php";
+
+    if (is_file($the_path) && !class_exists($class)) {
+        include $the_path;
+    }
 
     if (file_exists($the_path)) {
         require_once($the_path);
@@ -11,3 +15,6 @@ function __autoload($class) {
         die("This file name {$the_class}.php was not found!");
     }
 }
+
+// Any number of autoloader register the function
+spl_autoload_register('classAutoLoader');
