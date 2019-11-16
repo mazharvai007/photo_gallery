2c47e3e877281946f5132e7a12ae886946712d17
swopnil <swopnil420@gmail.com>
2019-11-17 00:06:45 +0600

    Create login page and check user signed in or not and if not, then checking username and password field value. If user is found then it's redirect to index page of admin area. Otherwise throwing a message.

---
+++ admin/includes/login.php
@@ -0,0 +1,25 @@
+<?php
+// Initialize the init.php file
+require_once("init.php");
+
+// Check first the user is signed in or not. If signed in then redirect to index page of admin area
+if ($session->is_signed_in()) {
+    redirect("index.php");
+}
+
+// Check the username and password field value
+if (isset($_POST['submit'])) {
+    $username = trim($_POST['username']);
+    $password = trim($_POST['password']);
+
+    // Make the method to check user database
+    if ($user_found) {
+        $session->login($user_found);
+        redirect("index.php");
+    } else {
+        $the_message = "Your username or usernme are not correct.";
+    }
+} else {
+    $username = "";
+    $password = "";
+}
