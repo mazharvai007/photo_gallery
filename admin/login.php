8576caa75e16004ad0316efb91c27fd0be5004d4
swopnil <swopnil420@gmail.com>
2019-11-17 06:40:56 +0600

    Make logout page

--- admin/login.php
+++ admin/login.php
@@ -33,11 +33,11 @@ if (isset($_POST['submit'])) {
     <form id="login-id" action="" method="post">
         <div class="form-group">
             <label for="username">Username</label>
-            <input type="text" class="form-control" name="username">
+            <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
         </div>
         <div class="form-group">
             <label for="password">Password</label>
-            <input type="password" class="form-control" name="password">
+            <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
         </div>
         <div class="form-group">
             <input type="submit" name="submit" value="Submit" class="btn btn-primary">
