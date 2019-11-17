e1bd5ae0571d9c23df75fa39f87525a5fa06aadd
swopnil <swopnil420@gmail.com>
2019-11-17 06:49:48 +0600

    Make message method

--- admin/includes/session.php
+++ admin/includes/session.php
@@ -5,12 +5,34 @@ class Session
 {
     private $signed_in;
     public $user_id;
+    public $message;
 
     // Make construct method
     function __construct()
     {
         session_start();
         $this->check_the_login();
+        $this->check_message();
+    }
+
+    // Make message method
+    public function message($msg = "")
+    {
+        if (!empty($msg)) {
+            $_SESSION['message'] = $msg;
+        } else {
+            return $this->message();
+        }
+    }
+
+    private function check_message()
+    {
+        if (isset($_SESSION['message'])) {
+            $this->message = $_SESSION['message'];
+            unset($_SESSION['message']);
+        } else {
+            $this->message = "";
+        }
     }
 
     // Make user signed in method to get the private property value to use anywhere
