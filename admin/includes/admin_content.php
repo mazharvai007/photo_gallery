20c094b6f6e864c62b2310d3c8bee02fee1be326
Mazharul Islam <swopnil420@gmail.com>
2019-11-25 00:39:12 +0600

    Insert data in the photo table and testing the create and find_all method

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -21,11 +21,11 @@
 //            }
 
             // Create new user
-            $user->username = "karim";
-            $user->password = "123";
-            $user->first_name = "Muhammad";
-            $user->last_name = "Karim";
-            $user->create();
+//            $user->username = "karim";
+//            $user->password = "123";
+//            $user->first_name = "Muhammad";
+//            $user->last_name = "Karim";
+//            $user->create();
 
             // Update user
 //            $user = User::find_by_id(4);
@@ -49,6 +49,27 @@
 //            $user->first_name = "Ahmed";
 //            $user->last_name = "Abdullah";
 //            $user->save();
+
+            
+            // Insert data in the photos table
+//            $photo->photo_title = "One morning, when Gregor Samsa woke from troubled dreams";
+//            $photo->photo_des = "One morning, when Gregor Samsa woke from troubled dreams, he found himself transformed in his bed into a horrible vermin. He lay on his armour-like back, and if he lifted his head a little he could see his brown belly, slightly domed and divided by arches into stiff sections. The bedding was hardly able to cover it and seemed ready to slide off any moment.";
+//            $photo->photo_filename = "image.jpg";
+//            $photo->photo_type = "jpg";
+//            $photo->photo_size = "15";
+//            $photo->create();
+
+            // Find data from the photos table
+            $photos = Photo::find_all();
+            foreach ($photos as $photo) {
+                echo $photo->photo_id, "<br>";
+                echo $photo->photo_title, "<br>";
+                echo $photo->photo_des, "<br>";
+                echo $photo->photo_filename, "<br>";
+                echo $photo->photo_type, "<br>";
+                echo $photo->photo_size, "<br>";
+                echo "<br>";
+            }
 
             ?>
 
--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -4,7 +4,7 @@
 class Photo extends DB_Object
 {
     protected static $db_table = "photos";
-    protected static $db_table_fields = array('photo_id', 'photo_title', 'photo_des', 'photo_filename', 'photo_type', 'photo_size');
+    protected static $db_table_fields = array('photo_title', 'photo_des', 'photo_filename', 'photo_type', 'photo_size');
     public $photo_id;
     public $photo_title;
     public $photo_des;
