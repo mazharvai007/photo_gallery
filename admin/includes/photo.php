93d96e1035c29e2e8190f946033b131ac87582c9
Mazharul Islam <swopnil420@gmail.com>
2019-11-25 06:26:12 +0600

    adding temporary image path, directory and error method for uploading images.

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -12,6 +12,20 @@ class Photo extends DB_Object
     public $photo_type;
     public $photo_size;
 
+    public $tmp_path;
+    public $upload_directory = "images";
+    public $custom_errors = array();
+    public $upload_errors = array(
+        UPLOAD_ERR_OK => "There is no error",
+        UPLOAD_ERR_INI_SIZE => "The Upload file exceeds the upload_max_filesize directive in php.ini",
+        UPLOAD_ERR_FORM_SIZE => "The uploaded file exceeds the max_file_size directive that was specified in the HTML",
+        UPLOAD_ERR_PARTIAL => "The uploaded file was only partially uploaded.",
+        UPLOAD_ERR_NO_FILE => "No file was uploaded",
+        UPLOAD_ERR_NO_TMP_DIR => "Missing a temporary folder",
+        UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
+        UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file uploaded."
+    );
+
 }
 
 $photo = new Photo();
