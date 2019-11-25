bd6c2f46d2781a4b95dc4bbfdca554b1d46b2d9d
Mazharul Islam <swopnil420@gmail.com>
2019-11-25 07:16:52 +0600

    Set file method

--- admin/includes/photo.php
+++ admin/includes/photo.php
@@ -14,7 +14,7 @@ class Photo extends DB_Object
 
     public $tmp_path;
     public $upload_directory = "images";
-    public $custom_errors = array();
+    public $errors = array();
     public $upload_errors = array(
         UPLOAD_ERR_OK => "There is no error",
         UPLOAD_ERR_INI_SIZE => "The Upload file exceeds the upload_max_filesize directive in php.ini",
@@ -25,6 +25,30 @@ class Photo extends DB_Object
         UPLOAD_ERR_CANT_WRITE => "Failed to write file to disk.",
         UPLOAD_ERR_EXTENSION => "A PHP extension stopped the file uploaded."
     );
+
+    /*
+     * SET File Method
+     * This is passing $_FILES['upload_file'] as an argument
+     */
+
+    public function set_file($file)
+    {
+
+        // Check the file is empty or not file or not array, and if not upload the file, then throwing error.
+        if (empty($file) || !$file || !is_array($file)) {
+            $this->errors[] = "There was no file uploaded here";
+            return false;
+        } elseif ($file['error'] != 0) {
+            $this->errors[] = $this->upload_errors[$file['error']];
+            return false;
+        } else {
+            $this->photo_filename = basename($file['name']);
+            $this->tmp_path = $file['tmp_name'];
+            $this->photo_type = $file['type'];
+            $this->photo_size = $file['size'];
+        }
+
+    }
 
 }
 
