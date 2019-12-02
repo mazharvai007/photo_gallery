239624e4eb501b328d5c3e7f91f2b728afb2cb33
swopnil <swopnil420@gmail.com>
2019-12-03 00:11:15 +0600

    Display user data in the user page of admin

--- admin/users.php
+++ admin/users.php
@@ -1,41 +1,81 @@
-<?php include("includes/header.php"); ?>
+<?php
+include("includes/header.php");
 
-        <!-- Navigation -->
-        <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+// If user is not signed in, the redirect to login page
+if (!$session->is_signed_in()) {
+    redirect("login.php");
+}
 
-            <?php include ("includes/top_nav.php"); ?>
-            <?php include ("includes/side_nav.php"); ?>
+// Find all photos
+$users = User::find_all();
+?>
+
+<!-- Navigation -->
+<nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+
+    <?php include ("includes/top_nav.php"); ?>
+    <?php include ("includes/side_nav.php"); ?>
 
 
-        </nav>
+</nav>
 
-        <div id="page-wrapper">
+<div id="page-wrapper">
 
-            <div class="container-fluid">
+    <div class="container-fluid">
 
-                <!-- Page Heading -->
-                <div class="row">
-                    <div class="col-lg-12">
-                        <h1 class="page-header">
-                            Welcome to Users
-                            <small>Admin</small>
-                        </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> Users
-                            </li>
-                        </ol>
-                    </div>
-                </div>
-                <!-- /.row -->
-
+        <!-- Page Heading -->
+        <div class="row">
+            <div class="col-lg-12">
+                <h1 class="page-header">
+                    Welcome to Users
+                    <small>Admin</small>
+                </h1>
+                <ol class="breadcrumb">
+                    <li>
+                        <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
+                    </li>
+                    <li class="active">
+                        <i class="fa fa-file"></i> Users
+                    </li>
+                </ol>
             </div>
-            <!-- /.container-fluid -->
-
         </div>
-        <!-- /#page-wrapper -->
+        <!-- /.row -->
 
-  <?php include("includes/footer.php"); ?>
+        <div class="row">
+            <div class="col-md-12">
+                <div class="table-responsive">
+                    <table class="table table-hover">
+                        <thead>
+                        <tr>
+                            <th>Id</th>
+                            <th>Photo</th>
+                            <th>Username</th>
+                            <th>First Name</th>
+                            <th>Last Name</th>
+                        </tr>
+                        </thead>
+
+                        <tbody>
+                        <?php foreach ($users as $user) : ?>
+                            <tr>
+                                <th><?php echo $user->id; ?></th>
+                                <th><img src="<?php echo $photo->user_image; ?>" alt="" class="img-responsive img-thumbnail"></th>
+                                <th><?php echo $user->username; ?></th>
+                                <th><?php echo $user->first_name; ?></th>
+                                <th><?php echo $user->last_name; ?></th>
+                            </tr>
+                        <?php endforeach; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+
+    </div>
+    <!-- /.container-fluid -->
+
+</div>
+<!-- /#page-wrapper -->
+
+<?php include("includes/footer.php"); ?>
