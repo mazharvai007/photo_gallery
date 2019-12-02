305d1ae6f90cc68951ea7e0e4dab9ac8ed2326e0
swopnil <swopnil420@gmail.com>
2019-12-02 23:24:05 +0600

    Integrated the TinyMCE editor

--- admin/includes/footer.php
+++ admin/includes/footer.php
@@ -7,6 +7,10 @@
     <!-- Bootstrap Core JavaScript -->
     <script src="js/bootstrap.min.js"></script>
 
+    <!-- TinyMCE Editor Integration -->
+  <script src="//cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+  <script>tinymce.init({selector:'textarea'});</script>
+
 </body>
 
 </html>
