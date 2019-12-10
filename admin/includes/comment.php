261833ae59818bfd8c8af3d6ec71108821bd5b90
swopnil <swopnil420@gmail.com>
2019-12-10 22:57:25 +0600

    Self instantiate the comment class in the create comment method

--- admin/includes/comment.php
+++ admin/includes/comment.php
@@ -10,7 +10,22 @@ class Comment extends DB_Object
     public $author;
     public $comment_body;
 
-}
+    // Self instantiate the comment class in the create comment method
+    public static function create_comment($photo_id, $author, $comment_body)
+    {
+        if (!empty($photo_id) && !empty($author) && !empty($comment_body)) {
 
-// Instantiate the Comment Class
-$comment = new Comment();
+            // Instantiate the Comment Class
+            $comment = new Comment();
+
+            $comment->photo_id = (int)$photo_id;
+            $comment->author = $author;
+            $comment->comment_body = $comment_body;
+
+            return $comment;
+        } else {
+            return false;
+        }
+    }
+
+}
