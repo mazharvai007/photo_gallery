cd8ceca61a9f8499934924844fb1181363d0f5f4
swopnil <swopnil420@gmail.com>
2019-12-15 09:56:14 +0600

    Make paginate class, added construct function and properties

---
+++ admin/includes/paginate.php
@@ -0,0 +1,15 @@
+<?php
+
+class Paginate
+{
+    public $page;
+    public $items_per_page;
+    public $items_total_count;
+
+    public function __construct($page = 1, $items_per_page = 4, $items_total_count = 0)
+    {
+
+    }
+}
+
+$paginate = new Paginate();
