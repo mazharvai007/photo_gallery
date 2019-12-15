a5901bce05b284d12d4b308aafb11a07aec34627
swopnil <swopnil420@gmail.com>
2019-12-15 11:02:07 +0600

    make next, previous, page total, has previous, has next and offset method

--- admin/includes/paginate.php
+++ admin/includes/paginate.php
@@ -12,6 +12,41 @@ class Paginate
         $this->items_per_page = (int)$items_per_page;
         $this->items_total_count = (int)$items_total_count;
     }
+
+    // Make next method to move the next page
+    public function next()
+    {
+        return $this->current_page + 1;
+    }
+
+    // Make previous method to move the previous page
+    public function previous()
+    {
+        return $this->current_page - 1;
+    }
+
+    // Make total page method
+    public function page_total()
+    {
+        return ceil($this->items_total_count / $this->items_per_page);
+    }
+
+    // If has previous page, then the method will work
+    public function has_previous()
+    {
+        return $this->previous() >= 1 ? true : false;
+    }
+
+    // If has next page, then the method will work
+    public function has_next()
+    {
+        return $this->next() <= $this->page_total() ? true : false;
+    }
+
+    public function offset()
+    {
+        return ($this->current_page - 1) * $this->items_per_page;
+    }
 }
 
 $paginate = new Paginate();
