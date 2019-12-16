e87905d206c73083f718f3616d1b2f41ccce28b2
swopnil <swopnil420@gmail.com>
2019-12-16 12:49:46 +0600

    Enable selection button with jQuery

---
+++ admin/js/script.js
@@ -0,0 +1,10 @@
+
+$(document).ready(function () {
+
+    $(".modal_thumbnails").click(function () {
+        $("#set_user_image").prop("disabled", false);
+    });
+
+    tinymce.init({selector:'textarea'});
+
+});
