dddd22fe64a1e35bc459cc6603646e53e08aae3c
swopnil <swopnil420@gmail.com>
2019-12-16 13:10:55 +0600

    get image name with jQuery

--- admin/js/script.js
+++ admin/js/script.js
@@ -4,14 +4,24 @@ $(document).ready(function () {
     var user_href;
     var user_href_splitted;
     var user_id;
+    var image_src;
+    var image_src_splitted;
+    var image_name;
 
     $(".modal_thumbnails").click(function () {
+
+        // Enable selection button
         $("#set_user_image").prop("disabled", false);
 
+        // Get user id
         user_href = $("#user-id").prop("href");
         user_href_splitted = user_href.split("=");
         user_id = user_href_splitted[user_href_splitted.length - 1];
-        alert(user_id);
+
+        // Get image name
+        image_src = $(this).prop("src");
+        image_src_splitted = image_src.split("/");
+        image_name = image_src_splitted[image_src_splitted.length - 1];
 
     });
 
