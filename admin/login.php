8720a0155a051454c1bba0d3da7154433f7ed694
swopnil <swopnil420@gmail.com>
2019-12-18 00:11:06 +0600

    Add CSS for login page

--- admin/login.php
+++ admin/login.php
@@ -31,18 +31,20 @@ if (isset($_POST['submit'])) {
 ?>
 
 <div class="col-md-4 col-md-offset-3">
-    <h4 class="bg-danger"><?php echo $the_message; ?></h4>
-    <form id="login_id" action="" method="post">
-        <div class="form-group">
-            <label for="username">Username</label>
-            <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
-        </div>
-        <div class="form-group">
-            <label for="password">Password</label>
-            <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
-        </div>
-        <div class="form-group">
-            <input type="submit" name="submit" value="Submit" class="btn btn-primary">
-        </div>
-    </form>
+    <div class="login-area">
+        <h4 class="bg-danger"><?php echo $the_message; ?></h4>
+        <form id="login_id" action="" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" class="form-control" name="username" value="<?php echo htmlentities($username); ?>" >
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" class="form-control" name="password" value="<?php echo htmlentities($password); ?>">
+            </div>
+            <div class="form-group">
+                <input type="submit" name="submit" value="Login" class="btn btn-primary">
+            </div>
+        </form>
+    </div>
 </div>
